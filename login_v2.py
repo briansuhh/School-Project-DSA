b4f39a4efdf2d232ffd5d0aa8e0181e766384a0f
ayumi <brianmaysebastian@gmail.com>
2023-10-31 00:42:16 +0800

    add adopt page

--- login_v2.py
+++ login_v2.py
@@ -1,8 +1,11 @@
 import customtkinter as ctk
 from customtkinter import *
 import pandas as pd
-from PIL import Image
+from PIL import Image, ImageTk
+from cat_description import pet_id, pet_name, breed, age, color, gender, size, description, image_path, availability, adopt_date, cat_df
 
+
+# ------------------------------------- Login -------------------------------------
 # Open the CSV file that will act as the database for the credentials
 df = pd.read_csv('credentials.csv')
 
@@ -10,6 +13,16 @@ df = pd.read_csv('credentials.csv')
 usernames = df['username'].tolist()
 passwords = df['password'].tolist()
 
+# ------------------------------------- Image Icons -------------------------------------
+logo_icon = ctk.CTkImage(Image.open("logo.png"))
+home_icon = ctk.CTkImage(Image.open("home.png"))
+adopt_icon = ctk.CTkImage(Image.open("adopt.png"))
+donate_icon = ctk.CTkImage(Image.open("donate.png"))
+back_icon = ctk.CTkImage(Image.open("back.png"))
+profile_icon = ctk.CTkImage(Image.open("profile.png"))
+cat_high = ctk.CTkImage(Image.open("cat_high.jpg"), size=(100, 100))
+
+# ------------------------------------- Functions -------------------------------------
 def check_login():
     entered_username = username_entry.get()
     entered_password = password_entry.get()
@@ -42,7 +55,19 @@ def donate_button_event():
     select_frame_by_name("donate", donate_indicate)
 
 def profile_button_event():
-    pass
+    select_frame_by_name("profile", profile_indicate)
+
+# def cat_button_event(image_path):
+#     select_frame_by_name("cat_description", cat_indicate)
+
+def cat_button_event(image_path):
+    select_frame_by_name("cat_description", cat_indicate)
+    
+    # Find the cat information based on the provided image_path
+    cat_info = cat_df[cat_df['image_path'] == image_path].iloc[0]
+    
+    # Example: Display cat information in the cat_description frame
+    cat_description_label.configure(text=f"Cat Name: {cat_info['pet_name']}\nBreed: {cat_info['breed']}\nAge: {cat_info['age']}\nColor: {cat_info['color']}\nGender: {cat_info['gender']}\nSize: {cat_info['size']}\nDescription: {cat_info['description']}\nAvailability: {cat_info['availability']}\nAdopt Date: {cat_info['adopt_date']}")
 
 def select_frame_by_name(name, indicator):
     # show indicator
@@ -55,28 +80,47 @@ def select_frame_by_name(name, indicator):
     else:
         home_frame.pack_forget()
     if name == "adopt":
-        adopt_frame.pack()
+        adopt_frame.pack(fill="both", expand=True, anchor=CENTER)
     else:
         adopt_frame.pack_forget()
     if name == "donate":
         donate_frame.pack()
     else:
         donate_frame.pack_forget()
+    if name == "profile":
+        profile_frame.pack()
+    else:
+        profile_frame.pack_forget()
+    if name == "cat_description":
+        cat_description.pack()
+    else:  
+        cat_description.pack_forget()
 
 def hide_indicators():
     home_indicate.configure(fg_color="light grey")
     adopt_indicate.configure(fg_color="light grey")
     donate_indicate.configure(fg_color="light grey")
-    
-# Open the image file using PIL
-logo_icon = ctk.CTkImage(Image.open("logo.png"))
-home_icon = ctk.CTkImage(Image.open("home.png"))
-adopt_icon = ctk.CTkImage(Image.open("adopt.png"))
-donate_icon = ctk.CTkImage(Image.open("donate.png"))
-back_icon = ctk.CTkImage(Image.open("back.png"))
-profile_icon = ctk.CTkImage(Image.open("profile.png"))
+    profile_indicate.configure(fg_color="light grey")
 
-# Set the default theme for the widgets
+def display_pictures():
+    # Calculate the number of columns based on the desired number of columns per row
+    num_columns = 3
+    current_column = 0
+
+    for index, row in cat_df.iterrows():
+        image = ctk.CTkImage(Image.open(row["image_path"]), size=(100, 100))
+        image_button = ctk.CTkButton(adopt_frame, text="", image=image, width=100, height=100, fg_color="transparent", hover_color="grey")
+        image_button.configure(command=lambda img_path=row["image_path"]: cat_button_event(img_path))  # Example event handler
+
+        # Use the grid manager to organize buttons in rows and columns
+        image_button.grid(row=index // num_columns, column=current_column, padx=10, pady=10)
+
+        # Update the current column, and if it reaches the specified number of columns, reset it and move to the next row
+        current_column += 1
+        if current_column == num_columns:
+            current_column = 0
+
+# ------------------------------------- Main -------------------------------------
 # ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 
@@ -84,9 +128,9 @@ ctk.set_default_color_theme("dark-blue")
 root = ctk.CTk()
 root.title("Login")
 # root.geometry("300x300")
-root.geometry("500x400") # changed the size of the geometry for the homepage
+root.geometry("530x400") # changed the size of the geometry for the homepage
 
-# Create the first page (login_page)
+# ------------------------------------- Login Page -------------------------------------
 login_page = ctk.CTkFrame(root)
 login_page.pack(fill="both", expand=True, anchor=CENTER)
 
@@ -109,7 +153,7 @@ login_button.place(relx=0.5, rely=0.70, anchor="center")
 result_label = ctk.CTkLabel(login_page, text="")
 result_label.place(relx=0.5, rely=0.80, anchor="center")
 
-# Create the second page (application)
+# ------------------------------------- Homepage -------------------------------------
 homepage = ctk.CTkFrame(root)
 
 # Create the navigation bar inside the homepage
@@ -123,7 +167,7 @@ home_button = ctk.CTkButton(nav_bar, text="Home", image=home_icon, command=home_
 home_button.pack(padx=10, pady=10)
 
 home_indicate = ctk.CTkLabel(nav_bar, text="", height=40, width=2, fg_color="light grey")
-home_indicate.place(x=3, y=50)
+home_indicate.place(x=3, y=52)
 
 adopt_button = ctk.CTkButton(nav_bar, text="Adopt", image=adopt_icon, command=adopt_button_event)
 adopt_button.pack(padx=10, pady=10)
@@ -135,7 +179,7 @@ donate_button = ctk.CTkButton(nav_bar, text="Donate", image=donate_icon, command
 donate_button.pack(padx=10, pady=10)
 
 donate_indicate = ctk.CTkLabel(nav_bar, text="", height=40, width=2, fg_color="light grey")
-donate_indicate.place(x=3, y=150)
+donate_indicate.place(x=3, y=148)
 
 
 logout_button = ctk.CTkButton(nav_bar, text="Logout", image=back_icon, command=go_back)
@@ -144,6 +188,9 @@ logout_button.pack(padx=10, pady=10, side = ctk.BOTTOM)
 profile_button = ctk.CTkButton(nav_bar, text="Profile", image=profile_icon, command=profile_button_event)
 profile_button.pack(padx=10, side = ctk.BOTTOM)
 
+profile_indicate = ctk.CTkLabel(nav_bar, text="", height=40, width=2, fg_color="light grey")
+profile_indicate.place(x=3, y=318)
+
 
 nav_bar.pack(side = ctk.LEFT)
 nav_bar.pack_propagate(False)
@@ -151,15 +198,30 @@ nav_bar.configure(width=100, height=400)
 
 main_frame = ctk.CTkFrame(homepage)
 
-# This is the homepage
+# This is the homepage frame
 home_frame = ctk.CTkFrame(main_frame, corner_radius=0, fg_color="transparent")
 lb = ctk.CTkLabel(home_frame, text="Welcome to Pusaa", font=("Arial", 20))
 lb.pack(padx=10, pady=10)
 
 # This is the adopt page
-adopt_frame = ctk.CTkFrame(main_frame, corner_radius=0, fg_color="transparent")
+adopt_frame = ctk.CTkScrollableFrame(main_frame, corner_radius=0, fg_color="transparent")
+
 lb = ctk.CTkLabel(adopt_frame, text="Adopt a pet", font=("Arial", 20))
+lb.grid(row=0, column=0, columnspan=3, padx=10, pady=10)    
+
+display_pictures()
+
+# ------------------------------------- Image description -------------------------------------
+cat_description = ctk.CTkFrame(main_frame, corner_radius=0, fg_color="transparent")
+lb = ctk.CTkLabel(cat_description, text="Cat Description", font=("Arial", 20))
 lb.pack(padx=10, pady=10)
+
+cat_indicate = ctk.CTkLabel(cat_description, text="", height=40, width=2, fg_color="light grey")
+cat_indicate.place(x=3, y=318)
+
+cat_description_label = ctk.CTkLabel(cat_description, text="", font=("Arial", 10))
+cat_description_label.pack(padx=10, pady=10)
+
 
 # This is the donate page
 donate_frame = ctk.CTkFrame(main_frame, corner_radius=0, fg_color="transparent")
@@ -173,7 +235,7 @@ lb.pack(padx=10, pady=10)
 
 main_frame.pack(side = ctk.LEFT)    
 main_frame.pack_propagate(False)
-main_frame.configure(width=400, height=400)
+main_frame.configure(width=6000, height=400)
 
 # display home_frame by default
 select_frame_by_name("home", home_indicate)
---
+++ trash_code.py
@@ -0,0 +1,47 @@
+
+# # ------------------------------------- Row 1 -------------------------------------
+# row1= ctk.CTkFrame(adopt_frame, corner_radius=0, fg_color="transparent")
+# # show all the cats available for adoption based on the list of cats in the database
+# # image_label = ctk.CTkLabel(row1, text= "", image=cat_high)
+# # image_label.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# image_button = ctk.CTkButton(row1, text="", image=cat_high, width=100, height=100, fg_color="transparent", hover_color="grey", command=cat_button_event)
+# image_button.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# image_button = ctk.CTkButton(row1, text="", image=cat_high, width=100, height=100, fg_color="transparent", hover_color="grey", command=cat_button_event)
+# image_button.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# image_button = ctk.CTkButton(row1, text="", image=cat_high, width=100, height=100, fg_color="transparent", hover_color="grey", command=cat_button_event)
+# image_button.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# row1.pack(side = ctk.TOP, padx=10, pady=10)
+
+# # ------------------------------------- Row 2 -------------------------------------
+# row2= ctk.CTkFrame(adopt_frame, corner_radius=0, fg_color="transparent")
+# # show all the cats available for adoption based on the list of cats in the database
+
+# image_button = ctk.CTkButton(row2, text="", image=cat_high, width=100, height=100, fg_color="transparent", hover_color="grey", command=cat_button_event)
+# image_button.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# image_button = ctk.CTkButton(row2, text="", image=cat_high, width=100, height=100, fg_color="transparent", hover_color="grey", command=cat_button_event)
+# image_button.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# image_button = ctk.CTkButton(row2, text="", image=cat_high, width=100, height=100, fg_color="transparent", hover_color="grey", command=cat_button_event)
+# image_button.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# row2.pack(side = ctk.TOP, padx=10, pady=10)
+
+# # ------------------------------------- Row 3 -------------------------------------
+# row3= ctk.CTkFrame(adopt_frame, corner_radius=0, fg_color="transparent")
+# # show all the cats available for adoption based on the list of cats in the database
+
+# image_button = ctk.CTkButton(row3, text="", image=cat_high, width=100, height=100, fg_color="transparent", hover_color="grey", command=cat_button_event)
+# image_button.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# image_button = ctk.CTkButton(row3, text="", image=cat_high, width=100, height=100, fg_color="transparent", hover_color="grey", command=cat_button_event)
+# image_button.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# image_button = ctk.CTkButton(row3, text="", image=cat_high, width=100, height=100, fg_color="transparent", hover_color="grey", command=cat_button_event)
+# image_button.pack(side = ctk.LEFT, padx=10, pady=10)
+
+# row3.pack(side = ctk.TOP, padx=10, pady=10)
